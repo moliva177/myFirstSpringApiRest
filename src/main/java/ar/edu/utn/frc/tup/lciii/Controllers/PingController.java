0911aa1bd5d61c20ad28910f398cbb66e7be0c45
405193-OLIVA <405193@tecnicatura.frc.utn.edu.ar>
2024-06-04 14:09:51 -0300

    ping controller

--- src/main/java/ar/edu/utn/frc/tup/lciii/Controllers/PingController.java
+++ src/main/java/ar/edu/utn/frc/tup/lciii/Controllers/PingController.java
@@ -1,4 +1,13 @@
 package ar.edu.utn.frc.tup.lciii.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
 }
