20b7fb2c4de1640124e48672d1fafbf766ca0ff5
405193-OLIVA <405193@tecnicatura.frc.utn.edu.ar>
2024-06-04 14:03:07 -0300

    initial commit

---
+++ src/main/java/ar/edu/utn/frc/tup/lciii/PrimerApiSringApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.tup.lciii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerApiSringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimerApiSringApplication.class, args);
+    }
+
+}
